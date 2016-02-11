9b31f75e829df36400e880e67166e6c68d78340a
airt <airt@users.noreply.github.com>
2016-02-11 14:02:17 +0800

    Update test.hs (11-15)

--- test.hs
+++ test.hs
@@ -1,6 +1,7 @@
 
 import Test.HUnit
 import H_99.H_01_10
+import H_99.H_11_20
 
 test01 = TestCase $ assertEqual "myLast"
   9
@@ -42,6 +43,31 @@ test10 = TestCase $ assertEqual "encode"
   [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
   $ encode "aaaabccaadeeee"
 
-tests = TestList [
-  test01, test02, test03, test04, test05,
-  test06, test07, test08, test09, test10]
+test11 = TestCase $ assertEqual "encodeModified"
+  [Multiple 4 'a',Single 'b',Multiple 2 'c',
+   Multiple 2 'a',Single 'd',Multiple 4 'e']
+  $ encodeModified "aaaabccaadeeee"
+
+test12 = TestCase $ assertEqual "decodeModified"
+  "aaaabccaadeeee"
+  $ decodeModified
+    [Multiple 4 'a',Single 'b',Multiple 2 'c',
+     Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+test13 = TestCase $ assertEqual "encodeDirect"
+  [Multiple 4 'a',Single 'b',Multiple 2 'c',
+   Multiple 2 'a',Single 'd',Multiple 4 'e']
+  $ encodeDirect "aaaabccaadeeee"
+
+test14 = TestCase $ assertEqual "dupli"
+  [1,1,2,2,3,3]
+  $ dupli [1, 2, 3]
+
+test15 = TestCase $ assertEqual "repli"
+  "aaabbbccc"
+  $ repli "abc" 3
+
+tests = TestList
+  [test01, test02, test03, test04, test05,
+   test06, test07, test08, test09, test10,
+   test11, test12, test13, test14, test15]
